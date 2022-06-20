734cb99269866c688c532f3d9d1ac29e7ed221fd
Iván Pondal <ivan.pondal@gmail.com>
2022-06-20 12:41:00 -0300

    Enable eslintrc

--- pages/invoices.tsx
+++ pages/invoices.tsx
@@ -1,9 +1,50 @@
-import { Container, Box, Typography, Grid } from "@mui/material";
+import { Container } from "@mui/material";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { AuthGuard } from "../src/auth/AuthGuard";
 import { CompanyDetailsGuard } from "../src/company/CompanyDetailsGuard";
+import parseQueryParam from "../src/integration/query-params";
 import { InvoiceListContainer } from "../src/invoices/InvoiceListContainer";
 
+const sortingFields = [
+  "total",
+  "dueDate",
+  "creationDate",
+  "companyName",
+] as const;
+type SortingFieldTuple = typeof sortingFields;
+type SortingField = SortingFieldTuple[number];
+
+type SortingParams = {
+  sortBy?: SortingField;
+  sortOrder?: "ASC" | "DESC";
+};
+
+function isSortingField(value: string): value is SortingField {
+  return sortingFields.includes(value as SortingField);
+}
+
 export default function InvoiceList() {
+  const router = useRouter();
+  const [sortingParams, setSortingParams] = useState<SortingParams>({});
+
+  useEffect(() => {
+    if (router.isReady) {
+      if (router.query.sortBy) {
+        const parsedSortBy = parseQueryParam(router.query.sortBy);
+
+        if (parsedSortBy && isSortingField(parsedSortBy)) {
+          setSortingParams((sortingParams) => {
+            return {
+              ...sortingParams,
+              sortBy: parsedSortBy,
+            };
+          });
+        }
+      }
+    }
+  }, [router.isReady, router.query.sortBy]);
+
   return (
     <AuthGuard>
       <CompanyDetailsGuard>
--- src/components/data-test-grid.tsx
+++ src/components/data-test-grid.tsx
@@ -6,18 +6,23 @@ import {
   GridCell,
   GridNoRowsOverlay,
 } from "@mui/x-data-grid";
+import React from "react";
 
 export const DataTestLoadingOverlay = () => (
   <GridLoadingOverlay data-test="loading-overlay" />
 );
 
-export const DataTestRow =
-  (id: string) =>
-  (props: React.HTMLAttributes<HTMLDivElement> & GridRowProps) =>
-    <GridRow data-test={`${id}-row-${props.rowId}}`} {...props} />;
+export const DataTestRow = (id: string) =>
+  React.memo(function DataTestRow(
+    props: React.HTMLAttributes<HTMLDivElement> & GridRowProps
+  ) {
+    return <GridRow data-test={`${id}-row-${props.rowId}}`} {...props} />;
+  });
 
-export const DataTestCell = (id: string) => (props: GridCellProps<any, any>) =>
-  <GridCell data-test={`${id}-${props.field}`} {...props} />;
+export const DataTestCell = (id: string) =>
+  React.memo(function DataTestCell(props: GridCellProps<any, any>) {
+    return <GridCell data-test={`${id}-${props.field}`} {...props} />;
+  });
 
 export const DataTestNoRowsOverlay = () => (
   <GridNoRowsOverlay data-test="empty-placeholder" />
